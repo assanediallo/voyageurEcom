e3f424b3fa769d09ec3400af5143bb88ad50a5ae
assanediallo <76051408+assanediallo@users.noreply.github.com>
2022-03-31 13:35:26 +0200

    Create main.c

---
+++ BranchAndBround/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BNB.h"
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+	MethodeBranchAndBround(GenererMat());
+    return 0;
+}
