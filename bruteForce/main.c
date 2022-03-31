9c990fbfac71ac2f8526b9d664be0a2dda52b850
assanediallo <76051408+assanediallo@users.noreply.github.com>
2022-03-31 13:31:58 +0200

    Create main.c

---
+++ bruteForce/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BF.h"
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+
+  MethodeBrutForce(GenererMat());
+
+    return 0;
+}
